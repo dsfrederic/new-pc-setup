3c46a96b9356b90f9511d4930bbc7fc465a88104
Frédéric De Smet <fredericdesmet@live.be>
2021-02-04 20:56:27 +0100

    Update

--- windows/programs.ps1
+++ windows/programs.ps1
@@ -4,16 +4,24 @@ choco upgrade chocolateygui -fy
 # Browsers
 choco upgrade googlechrome -fy
 
-# Coding
+# Source control
 choco upgrade git -fy # requires config
 choco upgrade poshgit
+
+# IDE, Text editors
+choco upgrade visualstudio2019professional -fy
 choco upgrade vscode -fy
+
+# Programming languages, frameworks,... 
 choco upgrade python -fy
-choco upgrade terraform -fy
 
 # Azure
 choco upgrade azure-cli -fy
+az extension add --name azure-devops
 choco upgrade microsoftazurestorageexplorer -fy
+
+# Infra as code
+choco upgrade terraform -fy
 
 # Containers
 choco upgrade docker-desktop -fy
@@ -21,7 +29,11 @@ choco upgrade kubernetes-helm -fy
 choco upgrade kubernetes-cli -fy
 choco upgrade lens -fy
 
+# Data 
+choco upgrade sql-server-management-studio
+
 # Utilities
+choco upgrade adobereader -fy
 choco upgrade wsl2 -fy
 choco upgrade wsl-ubuntu-2004 -fy
 choco upgrade 7zip.upgrade -fy
