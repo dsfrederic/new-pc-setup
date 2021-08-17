3dd457a43b89ce3131c43048ad50dd9c73d46409
Frédéric De Smet <fredericdesmet@live.be>
2021-08-17 09:26:42 +0200

    Added homebrew install command

--- macOS/brew.zsh
+++ macOS/brew.zsh
@@ -1,3 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew install --cask google-chrome
 brew install --cask visual-studio-code
 brew install gatsby-cli
