f2aa137f88ace0b356093480d341e3fc9e48e9a3
Frédéric De Smet <frederic.desmet@cegeka.com>
2024-04-05 08:49:57 +0200

    Added TF

--- macOS/install.sh
+++ macOS/install.sh
@@ -40,6 +40,11 @@ brew install docker-compose
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  ## Ohmyzsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+
+brew install powershell/tap/powershell ## Extra config required in VSCode
+
+brew install terraform
+
 ## Azure 
 brew install azure-cli 
 az init
